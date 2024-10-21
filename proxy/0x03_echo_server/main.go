c0100d3384abdca443f2b646d783d18719242dd5
Utugoed <petr.fanernyi@gmail.com>
2024-10-21 15:54:58 +0300

    Update main.go

--- proxy/0x03_echo_server/main.go
+++ proxy/0x03_echo_server/main.go
@@ -10,18 +10,21 @@ func echo(conn net.Conn) {
 	defer conn.Close()
 	buffer := make([]byte, 512)
 	for {
+		# Здесь переменную input_len видно во всей функции
 		input_len, err := conn.Read(buffer)
 		if err == io.EOF {
 			log.Println("Client has disconnected")
 			break
 		}
+		# Даже после иф
 		if err != nil {
 			log.Println("Unknown error")
 			break
 		}
 		log.Printf("Received %d bytes", input_len)
 
-		if _, err := conn.Write(buffer[0:input_len]); err != nil {
+		if a, err := conn.Write(buffer[0:input_len]); err != nil {
+			# А а видно только внутри иф
 			log.Println("Error occured during the echoing")
 		}
 	}
